751a3ccb6ae336a57d18ec6ef630d8466a2e7927
Podotto <grethelhalphen@gmail.com>
2026-04-09 19:12:45 -0500

    Se añadió función para contar el número de logins exitosos.

--- src/analyzer.py
+++ src/analyzer.py
@@ -7,3 +7,20 @@ def total_events():
             total += 1
 
     print(f"Total de eventos: {total}")
+    
+# Successful Logins
+def login_success():
+    total = 0
+
+    with open("logs.txt") as file_object:
+        for record in file_object:
+            line = record.strip().split(",")
+
+            event = line[2].strip()
+            success = line[3].strip()
+
+            if event == "LOGIN" and success == "SUCCESS":
+                total += 1
+
+    print(f"Logins exitosos: {total}")
+
