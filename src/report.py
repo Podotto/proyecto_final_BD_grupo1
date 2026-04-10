6c6ebd12fb67a517d0db82ca3ec9b84b23007707
acastinaar2002-sys <a.castinaar@gmail.com>
2026-04-09 20:16:41 -0500

    Se complemento la logica de usuarios sospechoso y la generacion del reporte de los usuarios sospechosos y los mas activos

--- src/report.py
+++ src/report.py
@@ -31,5 +31,44 @@ def create_report():
                 if user in fail_count:
                     fail_count[user] += 1
                 else:
-                    fail_count[user] = 1
+                    fail_count[user] = 1 
+                    
+    unique_total = 0
+    for user in set(user_list):
+        unique_total += 1
+
+    suspicious_list = []
+    for user in fail_count:
+        if fail_count[user] > 3:
+            suspicious_list.append(user)
+
+    sorted_users = sorted(user_count.items(), key=lambda item: item[1], reverse=True) 
+    
+    with open("reporte.txt", "w") as report:
+        report.write("REPORTE DE ANALISIS DE LOGS\n")
+        report.write("---------------------------\n")
+        report.write(f"Total de eventos: {total_events}\n")
+        report.write(f"Logins exitosos: {login_success}\n")
+        report.write(f"Logins fallidos: {login_fail}\n")
+        report.write(f"Usuarios únicos: {unique_total}\n\n")
+
+        report.write("Usuarios sospechosos:\n")
+        if suspicious_list == []:
+            report.write("No hay usuarios sospechosos.\n")
+        else:
+            for user in suspicious_list:
+                report.write(f"{user}\n")
+
+        report.write("\nTop 3 usuarios con más actividad:\n")
+        total = 0
+        for user, count in sorted_users:
+            if total < 3:
+                report.write(f"{user}: {count}\n")
+                total += 1
+
+    print("Reporte generado correctamente.")
+    
+
+    create_report()
+
         
